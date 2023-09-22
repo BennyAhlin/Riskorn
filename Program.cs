737cb767638538e801d488850313f4321c67bd1c
BennyAhlin <Benny.Ahlin@hotmail.com>
2023-09-22 11:25:49 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿var riskorn = 5;
+var ruta1 = 1;
+var rutatot = 1;
+    while (ruta1 < riskorn)
+    {
+        ruta1 = ruta1 * 2;
+        rutatot = rutatot + 1;
+    }
+    Console.WriteLine("Du hamnade på ruta: " + rutatot);
